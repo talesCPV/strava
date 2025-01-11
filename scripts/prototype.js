c3bcf7e84311af0abe6f55dbe30e520ad558fd21
talesCPV <tales@flexibus.com.br>
2025-01-11 10:19:53 -0300

    by_script

--- scripts/prototype.js
+++ scripts/prototype.js
@@ -40,7 +40,7 @@ String.prototype.viewXDate = function(){
     const str = this.valueOf()
     const period = Number(str.substring(11,13)) >= 12 ? 'PM' : 'AM'
     const time = period == 'AM' ? str.substring(11,16) : Number(str.substring(11,13)) - 12 +  str.substring(13,16)
-    return `${time} ${period} ${month[Number(str.substring(5,7))]}-${str.substring(8,10)},${str.substring(0,4)}`
+    return `${time} ${period} ${month[Number(str.substring(5,7))-1]}-${str.substring(8,10)},${str.substring(0,4)}`
 }
 
 /* DATE */
